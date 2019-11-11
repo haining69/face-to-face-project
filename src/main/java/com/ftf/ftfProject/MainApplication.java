5d4d4b5cb1091f476695af1db51b4543c01961b3
17609184675 <578499233@qq.com>
2019-11-11 22:43:26 +0800

    加了 jquery.cookie.js

--- src/main/java/com/ftf/ftfProject/MainApplication.java
+++ src/main/java/com/ftf/ftfProject/MainApplication.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class MainApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MainApplication.class, args);
     }
 
